b05ecdb61090ccdeb8e69645e56fa2a7a90b09db
sofigandulfo <sofigandulfo@gmail.com>
2024-02-19 02:07:03 -0300

    Web responsive

--- src/components/Main/MainAbout/MainAbout.jsx
+++ src/components/Main/MainAbout/MainAbout.jsx
@@ -1,14 +1,14 @@
 import "../../Main/Main.css";
 import "./MainAbout.css";
 import Image from "../../Image/Image";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { usePhotoContext } from "../../../hooks/usePhotoContext";
 
-function MainAbout() { 
+function MainAbout() {
   const { allPhotos } = usePhotoContext();
   const [randomImage, setRandomImage] = useState(null);
-  const defaultImage = 'https://firebasestorage.googleapis.com/v0/b/fridakala-58cf8.appspot.com/o/documentos%2FBEA52197-06C0-4EC7-9060-AD5075907479.jpg?alt=media&token=008ff3bb-ec0a-4463-aa8a-c12d65e3af6d';
-
+  const defaultImage =
+    "https://firebasestorage.googleapis.com/v0/b/fridakala-58cf8.appspot.com/o/documentos%2FBEA52197-06C0-4EC7-9060-AD5075907479.jpg?alt=media&token=008ff3bb-ec0a-4463-aa8a-c12d65e3af6d";
 
   useEffect(() => {
     if (allPhotos.length > 0) {
@@ -19,34 +19,32 @@ function MainAbout() {
     }
   }, [allPhotos]);
 
-
-
   return (
     <main className="about-main">
       <div className="about-content">
         <div className="about-text">
           <h1 className="about-title">Hello! I am Frida Kala</h1>
           <p className="about-paragraph">
-            Soy una perrita muy <strong>adorable</strong>, <strong>traviesa</strong>, <strong>tierna</strong>, <strong>juguetona</strong> y <strong>linda</strong>.
-            Pero que eso no te haga confiar, porque también soy muy <strong>rebelde</strong> y no
-            hago caso.
+            Soy una perrita muy <strong>adorable</strong>,{" "}
+            <strong>traviesa</strong>, <strong>tierna</strong>,{" "}
+            <strong>juguetona</strong> y <strong>linda</strong>. Pero que eso no
+            te haga confiar, porque también soy muy <strong>rebelde</strong> y
+            no hago caso.
             <br />
             <br />
-            Me encanta estar con mi familia, pero más que nada con mi  <strong>ama suprema</strong>. 
-            Cada vez que se va me pongo a llorar y molesto al
-            resto, pero cuando vuelve me pongo muy contenta.
+            Me encanta estar con mi familia, pero más que nada con mi{" "}
+            <strong>ama suprema</strong>. Cada vez que se va me pongo a llorar y
+            molesto al resto, pero cuando vuelve me pongo muy contenta.
           </p>
         </div>
         {randomImage ? (
-          <Image src={randomImage} className='image about-image' />
+          <Image src={randomImage} className="image about-image" />
         ) : (
           <Image src={defaultImage} className="image about-image" />
         )}
-
       </div>
     </main>
   );
 }
 
 export default MainAbout;
-
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,36 @@
-import "./Navbar.css";
-import { NavLink } from "../NavLink/NavLink";
-import Input from "../Input/Input";
+import { useState } from 'react';
+import { NavLink } from '../NavLink/NavLink';
+import Input from '../Input/Input';
+import './Navbar.css'; // Asegúrate de tener este archivo con los estilos necesarios
+import MenuIcon from '../../assets/Menu';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
       <nav className="navbar">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <Input></Input>
+        <ul className={`navbar-ul ${menuOpen ? 'active' : ''}`}>
+          <li className="navbar-li">
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li className="navbar-li">
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li className="navbar-li">
+            <Input />
+          </li>
+        </ul>
+        <div className="menu-icon" onClick={toggleMenu}>
+          <MenuIcon />
+        </div>
       </nav>
     </>
   );
 }
 
 export default Navbar;
-
